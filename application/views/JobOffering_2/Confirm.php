fea54a229e040d7a63da024a184c3c2b151803d5
panupong963 <k0811883329>
2018-06-11 02:43:52 +0700

    อีกรอบ

---
+++ application/views/JobOffering_2/Confirm.php
@@ -0,0 +1,24 @@
+<div class="content-wrapper">
+    <div class="container-fluid">
+<table class="table table-striped">
+<thead>
+  <tr align="center">
+    <th width="252">ชื่องาน</th>
+    <th width="86">รับรองผล</th>
+  </tr>
+</thead>
+<tbody>
+<tbody>
+  <tr align="center">
+    <th>ขนของ</th>
+    <th><a class="btn btn-info" href = "JobOffering_job_2">คลิก</a></th>
+  </tr>
+  <tr align="center">
+    <th>จัดเอกสาร</th>
+    <th><button type="button" class="btn btn-info">คลิก</button></th>
+  </tr>
+</tbody>
+</table>
+
+    </div>
+</div>
---
+++ application/views/JobOffering_2/DataST.php
@@ -0,0 +1,33 @@
+<div class="content-wrapper">
+    <div class="container-fluid">
+<table class="table table-bordered">
+  <thead>
+  <tr align="center">
+    <td width="58">ลำดับ</td>
+    <td width="416">สำนักวิชา</td>
+    <td width="185">จำนวน นศ. ที่ยืนยัน</td>
+    <td width="213">จำนวน นศ. ที่ไม่ยืนยัน</td>
+  </tr>
+</thead>
+<tbody>
+  <tr align="center">
+    <td>1</td>
+    <td>สารสนเทศศสาตร์</td>
+    <td><button type="button" class="btn btn-link">10</button></td>
+    <td><button type="button" class="btn btn-link">20</button></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr align="center">
+    <td colspan="2">รวม</td>
+    <td>10</td>
+    <td>20</td>
+  </tr>
+</tbody>
+</table>
+</div>
+</div>
