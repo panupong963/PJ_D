d654a474a297755d81a197fd5ebc3628f96112ac
buradsagorn <ford_buradsagorn@hotmail.com>
2018-06-15 18:07:23 +0700

    Revert "ford"
    This reverts commit 20f897776eb0faa4b397b54e0370dc2be28b7905.

--- application/views/Authorities_1/NewDeclare.php
+++ application/views/Authorities_1/NewDeclare.php
@@ -5,11 +5,9 @@
   <thead>
   <tr align="center">
     <th width="58">ลำดับ</th>
-    <th width="130">รหัสนักศึกษา</th>
+    <th width="110">รหัสนักศึกษา</th>
     <th width="270">ชื่อ - สกุล</th>
-    <th width="200">หลักสูตร</th>
-    <th width="150">ประเภทงาน</th>
-    <th width="200">ชื่องาน</th>
+    <th width="270">หลักสูตร</th>
     <th width="158">ผลการพิจารณา</th>
   </tr>
     </thead>
@@ -19,18 +17,14 @@
     <th>58888888</th>
     <th>สมบูรณ์ ใจเย็น</th>
     <th>เทคโนโลยีสารสนเทศ</th>
-    <th>บริการ</th>
-    <th>ยืมคืนหนังสือ</th>
     <th>ผ่าน</th>
   </tr>
-   <tr align="center">
-    <th>2</th>
-    <th>58888890</th>
-    <th>สมหวัง ขาวสะอาด</th>
-    <th>พยาบาลศาสตร์</th>
-    <th>บริการ</th>
-    <th>จัดส่งเอกสาร</th>
-    <th>ผ่าน</th>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
   </tr>
 </tbody>
 </table>
