47539bb196c424943265618d589f474bd7493e87
buradsagorn <ford_buradsagorn@hotmail.com>
2018-09-10 12:28:56 +0700

    ll

--- application/views/Finance_1/WriteDescription.php
+++ application/views/Finance_1/WriteDescription.php
@@ -285,11 +285,12 @@
                         </table>
 
                 <tr>
-                <td><button class="btn btn-info btn-lg pull-right" type="button"  style="width:90px;height:40px"<a href="localhost/PJ_D/index.php/home2/finance_WriteDescription_1" ><h6>ย้อนกลับ</h6></button>
+                <td>
+                 <button class="btn btn-info nextBtn btn-lg pull-right" type="button"  style="width:68px;height:40px" ><h6>บันทึก</h6></button></td>
 
-                 </td><td>
-                 <button class="btn btn-info nextBtn btn-lg pull-right" type="button"  style="width:70px;height:40px" ><h6>บันทึก</h6></button></td>
-             
+             <td><button class="btn btn-info btn-lg pull-right" type="button"  style="width:88px;height:40px"<a href="localhost/PJ_D/index.php/home2/finance_WriteDescription_1" ><h6>ย้อนกลับ</h6></button>
+
+                 </td>
            
                 </tr>
                 </table>
